02950cbf608bd5f651c95c61435b393a7f56156f
Alex Johnson <alex.johnson@twinspires.com>
2017-07-11 11:49:53 -0400

    part 5-1

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+
 import { HeroDetailComponent} from './hero-detail.component';
 import { Hero } from '../classes/hero';
+import { HeroService } from '../services/hero.service';
 
 @Component({
   selector: 'my-app',
+  providers: [HeroService],
   template: `
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
@@ -67,27 +70,18 @@ import { Hero } from '../classes/hero';
   `]
 })
 export class AppComponent {
+
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  heroes = HEROES;
+  heroes : Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService){ }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHerores();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   };
 }
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-]
